5b4ec9832e093529b447ed0d0b841701a8b20d04
Ravi Teja <rnkbw@umsystem.edu>
2021-04-29 00:58:08 -0500

    ICP13

--- Mobile/ICP13/Source/SpeechToText/app/src/main/java/com/vijaya/speechtotext/MainActivity.java
+++ Mobile/ICP13/Source/SpeechToText/app/src/main/java/com/vijaya/speechtotext/MainActivity.java
@@ -1,59 +1,66 @@
 package com.vijaya.speechtotext;
-
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
-import android.content.SharedPreferences;
-import android.speech.tts.TextToSpeech;
-import java.text.SimpleDateFormat;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQ_CODE_SPEECH_INPUT = 100;
     private TextView mVoiceInputTv;
     private ImageButton mSpeakBtn;
-    private TextToSpeech textToSpeech;
+    TextToSpeech tts;
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
-    private static final String PREFS = "prefs";
-    private static final String NAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        preferences = getSharedPreferences(PREFS,0);
-        editor = preferences.edit();
-
+        // Fetching text view, speak button by Id's
         mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
         mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
-        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
 
+        // Initializing Preferences, Editor
+        preferences = getSharedPreferences("namePrefs",0);
+        editor = preferences.edit();
+
+        // Say Hello First on Page Load
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    // Setting the Locale.
+                    tts.setLanguage(Locale.US);
+                    tts.speak("Hello", TextToSpeech.QUEUE_FLUSH, null);
+                    mVoiceInputTv.setText(Html.fromHtml("<p style=\"color:red;\">Speaker : Hello !!</p>"));
+                }
+            }
+        });
+        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startVoiceInput();
             }
         });
-
-        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    int text = textToSpeech.setLanguage(Locale.UK);
-                    textToSpeech.speak("Hello", TextToSpeech.QUEUE_FLUSH, null);
-                } }});
     }
 
     private void startVoiceInput() {
+        // Starting Voice Input on click of Mic button
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
@@ -61,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
-
+            a.printStackTrace();
         }
     }
 
@@ -71,44 +78,50 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case REQ_CODE_SPEECH_INPUT: {
                 if (resultCode == RESULT_OK && null != data) {
+                    // Fethcing the result
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    mVoiceInputTv.setText(result.get(0));
-                    ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    String input = res.get(0);
-                    assistant(input);
+                    if(result != null && result.size() > 0) {
+                        mVoiceInputTv.append(Html.fromHtml("<p style=\"color:blue;\">User : "+result.get(0)+"</p>"));
+                        // If user says hello, Ask for User's name & show the Greeting Text with users name.
+                        if(result.get(0).equalsIgnoreCase("hello")) {
+                            tts.speak("What is your name", TextToSpeech.QUEUE_FLUSH, null);
+                            mVoiceInputTv.append(Html.fromHtml("<p style=\"color:red;\">Speaker : What is your name ?</p>"));
+                        }else if(result.get(0).contains("name")){
+                            // Set the Greeting by indexing
+                            String name = result.get(0).substring(result.get(0).lastIndexOf(' ') + 1);
+                            // Setting into Editor
+                            editor.putString("name", name).apply();
+                            tts.speak("Hello, "+name,
+                                    TextToSpeech.QUEUE_FLUSH, null);
+                            mVoiceInputTv.append(Html.fromHtml("<p style=\"color:red;\">Speaker : Hello, "+name+"</p>"));
+                        }else if(result.get(0).contains("not feeling good")){
+                            tts.speak("I can understand. Please tell your symptoms in short",
+                                    TextToSpeech.QUEUE_FLUSH, null);
+                            mVoiceInputTv.append(Html.fromHtml("<p style=\"color:red;\">Speaker : I can understand. Please tell your symptoms in short</p>"));
+                        }else if(result.get(0).contains("thank you")){
+                            tts.speak("Thank you too, "+preferences.getString("name","")+" Take care.", TextToSpeech.QUEUE_FLUSH, null);
+                            mVoiceInputTv.append(Html.fromHtml("<p style=\"color:red;\">Speaker : Thank you too, "+preferences.getString("name","")+" Take care.</p>"));
+                        }else if(result.get(0).contains("what time")){
+                            // Speaking the Time for the User
+                            SimpleDateFormat sdfDate =new SimpleDateFormat("HH:mm");//dd/MM/yyyy
+                            Date now = new Date();
+                            String[] strDate = sdfDate.format(now).split(":");
+                            if(strDate[1].contains("00"))strDate[1] = "o'clock";
+                            tts.speak("The time is : "+sdfDate.format(now), TextToSpeech.QUEUE_FLUSH, null);
+                            mVoiceInputTv.append(Html.fromHtml("<p style=\"color:red;\">Speaker : The time is : "+sdfDate.format(now)+"</p>"));
+                        }else if(result.get(0).contains("medicine")){
+                            tts.speak("I think you have fever. Please take this medicine.",
+                                    TextToSpeech.QUEUE_FLUSH, null);
+                            mVoiceInputTv.append(Html.fromHtml("<p style=\"color:red;\">Speaker : I think you have fever. Please take this medicine.</p>"));
+                        } else {
+                            tts.speak("Sorry, I cant help you with that", TextToSpeech.QUEUE_FLUSH, null);
+                            mVoiceInputTv.append(Html.fromHtml("<p style=\"color:red;\">Speaker : Sorry, I cant help you with that</p>"));
+                        }
+                    }
                 }
                 break;
             }
+
         }
     }
-
-    private void assistant(String text){
-        Log.e("Speech","" + text);
-        String[] speech = text.split(" ");
-        if(text.contains("hello")){
-            textToSpeech.speak("What is your name?", TextToSpeech.QUEUE_FLUSH, null);
-        }
-        if(text.contains("hi my name is ")){
-            String name = speech[speech.length-1];
-            editor.putString(NAME,name).apply();
-            textToSpeech.speak("How are you today?" + preferences.getString(NAME, null), TextToSpeech.QUEUE_FLUSH, null);
-        }
-        else if(text.contains("I'm not feeling good what should I do")){
-            textToSpeech.speak("I can understand. Please tell your symptoms in short.", TextToSpeech.QUEUE_FLUSH, null);
-        }
-        else if(text.contains("what medicine should I take")) {
-            textToSpeech.speak(preferences.getString(NAME, null) + ", I think you have Fever. Please take this medicine.", TextToSpeech.QUEUE_FLUSH, null);
-        }
-        else if(text.contains("thank you")){
-            textToSpeech.speak("Thank you too" + preferences.getString(NAME, null)+"Take care", TextToSpeech.QUEUE_FLUSH, null);
-        }
-        else if(text.contains("what time is it")) {
-            SimpleDateFormat sdfDate =new SimpleDateFormat("h:mm a");
-            Date now = new Date();
-            String[] strDate = sdfDate.format(now).split(":");
-            if(strDate[1].contains("00"))strDate[1] = "o'clock";
-            textToSpeech.speak("The time is : "+sdfDate.format(now), TextToSpeech.QUEUE_FLUSH, null);
-        }
-    }
-
 }
